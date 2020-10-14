831183b6f0a090957f3b62767527541eb935bed6
Evan Hayes <evanhayes@Evans-MBP.fios-router.home>
2020-10-14 17:59:16 -0400

    enter

---
+++ ppmtool-react/src/components/DashBoard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react'
+import ProjectItem from '../project/ProjectItem'
+
+class Dashboard extends Component {
+    render() {
+        return ( <div >           
+            <ProjectItem/>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
